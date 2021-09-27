56f8dd94c1e67b6682f802d925d723033bcdb06c
Davidroljim <davidroldan2901@gmail.com>
2021-09-27 12:08:13 +0200

    Primera actividad

---
+++ ejercicio1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio1</title>
+</head>
+<body>
+    <?php
+    $nombre = "David Roldán Jiménez";
+    $ciudad = "Sevilla";
+    $edad = 19;
+    printf ("Me llamo %s vivo en %s y tengo %d años",$nombre, $ciudad, $edad);
+
+    print "</br>";
+
+    echo "Me llamo David Roldán Jiménez vivo en Sevilla y tengo 19 años";
+    ?>
+</body>
+</html>
